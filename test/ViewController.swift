6f0b8b70c9df462a83ad305328c763873e1097fb
Dmitry Burtsev <kerberos464@gmail.com>
2018-10-20 05:20:53 -0700

    start commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,12 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var stillTyping = false
+    var firstOperand: Double = 0
+    var secondOperand: Double = 0
+    var operationSign: String = ""
+    var currentInput: Double {
+        get {
+            return Double(displayResultLabel.text!)!
+        }
+        set {
+            var outputValue = "\(newValue)"
+            var valueArray = outputValue.components(separatedBy: ".")
+            if valueArray[1] == "0" {
+                outputValue = valueArray[0]
+            }
+            displayResultLabel.text! = outputValue
+            stillTyping = false
+        }
+    }
+    
+    @IBOutlet weak var displayResultLabel: UILabel!
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        if stillTyping {
+            if displayResultLabel.text!.count < 20 {
+                displayResultLabel.text! = displayResultLabel.text! + number
+            }
+        } else {
+            displayResultLabel.text = number
+            stillTyping = true
+        }
+    }
+    
+    @IBAction func twoOperandsButtonPressed(_ sender: UIButton) {
+        firstOperand = currentInput
+        stillTyping = false
+        operationSign = sender.currentTitle!
+    }
+    
+    func operateWithTwoOperands(operation: (Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperand)
+        stillTyping = false
+    }
+    
+    @IBAction func equalitySignPressed(_ sender: UIButton) {
+        if stillTyping {
+            secondOperand = currentInput
+        }
+        
+        switch operationSign {
+        case "+":
+            operateWithTwoOperands{$0 + $1}
+        case "-":
+            operateWithTwoOperands{$0 - $1}
+        case "✕":
+            operateWithTwoOperands{$0 * $1}
+        case "÷":
+            operateWithTwoOperands{$0 / $1}
+        default:
+            break
+        }
     }
 
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        stillTyping = false
+        firstOperand = 0
+        secondOperand = 0
+        currentInput = 0
+        operationSign = ""
+    }
+
+    @IBAction func squareRootButtonPressed(_ sender: UIButton) {
+        currentInput = sqrt(currentInput)
+    }
+    
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+        currentInput = -currentInput
+    }
+
+    @IBAction func percentButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
-
